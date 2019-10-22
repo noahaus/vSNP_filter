83c2bffdbc93be6656a79453587fe44f53cf7830
Noah Legall <40697188+noahaus@users.noreply.github.com>
2019-10-22 12:52:26 -0400

    debug

--- vSNP_filter.sh
+++ vSNP_filter.sh
@@ -1,5 +1,5 @@
 #use R to read and filter data, python to keep the vSNP output formatting
 CSV=$1
 CUTOFF=$2
-Rscript filter_regions.R $CSV $CUTOFF
-python excel_writer.py pe-ppe_filtered_out.csv
+Rscript vSNP_filter/filter_regions.R $CSV $CUTOFF
+python vSNP_filter/excel_writer.py ./pe-ppe_filtered_out.csv
